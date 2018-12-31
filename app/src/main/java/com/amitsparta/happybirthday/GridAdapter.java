25692c152bbf334a3f994d20cafbdf4d5872800f
Tushar <tusharsharmaji23@gmail.com>
2018-12-31 13:14:45 +0530

    Adapter ready.

--- app/src/main/java/com/amitsparta/happybirthday/GridAdapter.java
+++ app/src/main/java/com/amitsparta/happybirthday/GridAdapter.java
@@ -3,6 +3,7 @@ package com.amitsparta.happybirthday;
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
@@ -12,42 +13,43 @@ import java.util.ArrayList;
 
 public class GridAdapter extends RecyclerView.Adapter<GridAdapter.GridItemHolder> {
 
-    private ArrayList arrayList;
+    private ArrayList itemList;
     private Context context;
 
-    public GridAdapter(Context context, ArrayList arrayList) {
+    public GridAdapter(Context context, ArrayList itemList) {
         this.context = context;
-        this.arrayList = arrayList;
+        this.itemList = itemList;
     }
 
     @NonNull
     @Override
     public GridItemHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View view = LayoutInflater.from(context).inflate(R.layout.grid_item, parent);
+        return new GridItemHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull GridItemHolder holder, int position) {
-
+        holder.displayItem((DataFile) itemList.get(position));
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return itemList.size();
     }
 
-    public class GridItemHolder extends RecyclerView.ViewHolder {
+    class GridItemHolder extends RecyclerView.ViewHolder {
 
         private View itemView;
 
-        public GridItemHolder(View itemView) {
+        GridItemHolder(View itemView) {
             super(itemView);
             this.itemView = itemView;
         }
 
         void displayItem(DataFile item) {
-            ImageView image = (ImageView)itemView.findViewById(R.id.file_image);
-            TextView fileName = (TextView)itemView.findViewById(R.id.file_name);
+            ImageView image = itemView.findViewById(R.id.file_image);
+            TextView fileName = itemView.findViewById(R.id.file_name);
 
             image.setImageBitmap(item.getImage());
             fileName.setText(item.getFilePath());
